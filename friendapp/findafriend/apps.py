8bd2726dba8db37351096583a013b37a1f05e780
chai-v <99544705+chai-v@users.noreply.github.com>
2022-07-09 07:44:27 +0530

    Delete friendapp directory

--- friendapp/findafriend/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FindafriendConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'findafriend'
--- friendapp/findafriend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns=[
-    path('', views.landing, name='landing'),
-    path('login', views.login, name='login'),
-    path('signup', views.signup, name='signup'),
-    path('browse', views.browse, name='browse')
-]
